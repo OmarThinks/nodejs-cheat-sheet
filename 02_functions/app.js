7391c7d38b4a2677452da5dfc3991a3d30e93ad1
OmarThinks <contact.parmag@gmail.com>
2021-02-04 18:54:48 +0200

    functions:first way

--- 02_functions/app.js
+++ 02_functions/app.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+/*
+This is the first way to create a function
+*/
+function hello1()
+{
+	console.log("Hello, World!");
+}
+console.log("________________________________ Function No. 1:")
+hello1();
+
+
+
+
+
+
