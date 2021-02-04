f1c911cad117801d7e260abb4d557312597ae194
OmarThinks <contact.parmag@gmail.com>
2021-02-04 20:36:49 +0200

    functions:add:documentation

--- 02_functions/app.js
+++ 02_functions/app.js
@@ -1,5 +1,6 @@
 /*
-This is the first way to create a function
+# 1
+Normal function
 */
 console.log("________________________________ 1) Very simple function:")
 function hello1()
@@ -7,59 +8,56 @@ function hello1()
 	console.log("Hello, World!");
 }
 hello1();
-
+// Hello, World!
 
 
 /*
-The second way to create a function:
-With paramaters:
+# 2:
+Function with paramaters:
 */
-
 console.log("________________________________ 2) Function with paramters")
 function hello2(theName)
 {
 	console.log("Hello, "+theName+"!");
 }
-
 hello2("Mohammed");
-
+// Hello, Mohammed!
 
 
 /*
-The third way to create a function:
+# 3
 Function expression:
 */
-
-
 console.log("________________________________ 3) Function expression")
 var hello3 = function(theName)
 {
 	console.log("Hello again, "+theName+"!");
 }
-
 hello3("User");
-
-
+// Hello again, User!
 
 
 /*
-The fourth way to create a function:
+# 4
 Function expression:
 */
-
-
 console.log("________________________________ 4) Arrow Function")
 var hello4 = (theName) => 
 {
 	console.log("Hello again and again, "+theName+"!");
 }
-
-
 hello4("Userrrr");
+// Hello again and again, Userrrr!
 
 
-
-
-
-
-
+/*
+# 5
+Function in a function:
+*/
+console.log("________________________________ 5) Function in a function")
+function innerFunction(func) 
+{
+	func();
+}
+innerFunction(hello1);
+// Hello, World!
