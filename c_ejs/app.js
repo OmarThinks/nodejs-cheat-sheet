03b48305bf863b376fef7b5ea5990ada91aaff4c
OmarThinks <contact.parmag@gmail.com>
2021-02-11 10:17:31 +0200

    sample.ejs:created

---
+++ c_ejs/app.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+var corsOptions = {
+    "origin": "*",
+    "methods": "GET,PUT,PATCH,POST,DELETE",
+    "allowedHeaders": ['Content-Type', 'Authorization']
+}
+
+app.use(cors(corsOptions));
+app.set("view engine", "ejs");
+
+
+
+
+app.listen(3000)
